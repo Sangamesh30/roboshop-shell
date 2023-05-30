63a608ca16f28906976810e9d81f001eb1294aa4
Sangamesh30 <sangamesh.3094@gmail.com>
2023-05-30 18:21:50 +0530

    roboshop nginx script

--- catalogue.sh
+++ catalogue.sh
@@ -1,12 +1,16 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
+
 useradd roboshop
 mkdir /app
+
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
+
 cd /app
 npm install
+
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
 systemctl daemon-reload
--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,6 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 ## we need to copy config file
-cp /home/centos/roboshop-shell/roboshop.config /etc/nginx/default.d/roboshop.conf
+cp /home/centos/roboshop-shell/roboshop.config /etc/nginx/default.d/roboshop.config
 systemctl enable nginx
 systemctl restart nginx
