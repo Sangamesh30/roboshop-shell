253a2b437a118fb3594f6e4e2badf8d7c228bd02
Sangamesh30 <sangamesh.3094@gmail.com>
2023-05-30 14:15:05 +0530

    roboshop mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@ cp mongodb.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
-#modifiy the config file
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
 systemctl restart mongod
