8d6bee47c0083f6c76d32c1db42ec44b3cf69109
Sangamesh30 <sangamesh.3094@gmail.com>
2023-05-31 00:40:20 +0530

    roboshop nginx script

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 
-#update the listen address
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
 systemctl restart mongod
