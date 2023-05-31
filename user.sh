9a3d1e4b4a34c14536dc7e6217496106bf392485
Sangamesh30 <sangamesh.3094@gmail.com>
2023-05-31 08:25:13 +0530

    user component

---
+++ user.sh
@@ -0,0 +1,28 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+
+cd /app
+npm install
+
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+
+#config file
+systemctl daemon-reload
+
+systemctl enable user
+systemctl restart user
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org-shell -y
+
+mongo --host mongodb-dev.awsdevops73.store </app/schema/user.js
